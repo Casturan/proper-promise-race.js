fb913515e80e30fca97e394403957dde6ecc1f1f
Casturan <casturan@gmail.com>
2016-12-18 14:14:50 +0100

    Create proper-race.js

---
+++ proper-race.js
@@ -0,0 +1,20 @@
+Promise.properRace = function(promises, count = 1, results = []) {
+  promises = Array.from(promises);
+  if (promises.length < count) {
+    return Promise.reject('Race is not finishable');
+  }
+   
+  let indexPromises = promises.map((p, index) => p.then(() => index, () => {throw index;}));
+   
+  return Promise.race(indexPromises).then(index => {
+    let p = promises.splice(index, 1)[0];
+    results.push(p);
+    if (count === 1) {
+      return results;
+    }
+    return Promise.properRace(promises, count-1, results);
+  }, index => {
+    promises.splice(index, 1);
+    return Promise.properRace(promises, count, results);
+  });
+};
